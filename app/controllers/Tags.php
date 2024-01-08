97ce63934a9d66af1b62c9139674d8c29ef51192
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-01-08 19:36:08 +0100

    crud tag show and create functions added

--- app/controllers/Tags.php
+++ app/controllers/Tags.php
@@ -7,5 +7,41 @@ class Tags extends Controller {
         $tags = $this->tagModel->showTags();
         $this->view('admin/tags',['tags'=>$tags]);
     }
+    public function create(){
+        if (isset($_POST["addTag"])) {
+            if ($_POST['tagName'] == ' ') {
+                $name_error = 'Invalid Tag Name';
+            } else {
+                $name_error = '';
+            }
+            if ($name_error == '') {
+                $this->tagModel->getTag()->setName(trim($_POST['tagName']));
+                if ($this->tagModel->create($this->tagModel->getTag())) {
+                    $this->view('admin/tag');
+                } else {
+                    $error_tag = [
+                        'name_error' => $name_error,
+                    ];
+                    header('location: http://localhost/wikiApp/tags');
+
+                }
+            } else {
+                $error_tag = [
+                    'name_error' => $name_error,
+                ];
+                $this->view('admin/dashboard', $error_tag);
+            }
+        }
+    }
+    public function delete(){
+        if (isset($_POST["deleteTag"])) {
+
+            $this->tagModel->getTag()->setId(trim($_POST['idTag']));
+            if ($this->tagModel->delete($this->tagModel->getTag())) {
+                header('location: http://localhost/wikiApp/tags');
+            }
+
+        }
+    }
 
 }
--- app/models/TagDAO.php
+++ app/models/TagDAO.php
@@ -27,13 +27,23 @@ class TagDAO
         $tags = array();
         while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
             $tag = new Tag();
-
             $tag->setId($row['tagId']);
             $tag->setName($row['tagName']);
-
             array_push($tags, $tag);
         }
         return $tags;
     }
-
+    public function create(Tag $tag){
+        $name = $tag->getName();
+        $stmt = $this->conn->prepare("INSERT INTO tag (tagName) VALUES (:name)");
+        $stmt->bindParam(':name', $name);
+        $stmt->execute();
+    }
+    public function delete(Tag $tag){
+        $id = $tag->getId();
+        $query = "DELETE FROM tag WHERE tagId = :Id";
+        $statement = $this->conn->prepare($query);
+        $statement->bindParam(':Id', $id, PDO::PARAM_INT);
+        $statement->execute();
+    }
 }
